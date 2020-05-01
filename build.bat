e5b51061ea9af646376d6e3542a3b446945e42cb
dymaxionkim <dymaxion.kim@gmail.com>
2020-05-01 20:20:41 +0900

    Fix Windows Script

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
 pyinstaller --onefile --noconsole --icon=g2m.ico g2m.py
+copy dist\g2m.exe g2m.exe
--- g2m.bat
+++ g2m.bat
@@ -1,2 +1,2 @@
 g2m.exe
-start chrome --headless --incognito --disable-gpu --window-size=1600,900 --screenshot=gantt.png gantt.html
+start chrome --headless --incognito --disable-gpu --window-size=1600,900 --screenshot=C:/git/G2M/gantt.png C:/git/G2M/gantt.html
