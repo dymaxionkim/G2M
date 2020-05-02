1055a047d6f3dd964f9c6c3fa18ab7eb13e458d5
dymaxionkim <dymaxion.kim@gmail.com>
2020-05-02 19:22:44 +0900

    Add sleep 2

--- g2m.sh
+++ g2m.sh
@@ -13,18 +13,22 @@ mv ./gantt.png ./gantt.png.old
 
 # Make gantt.csv
 /usr/bin/ganttproject -export csv gantt.gan
+sleep 2
 echo "Done for gantt.csv"
 
 # Make gantt.png
 /usr/bin/ganttproject -export png gantt.gan
+sleep 2
 echo "Done for gantt.png"
 
 # Make gantt.html
 python ./g2m.py
+sleep 2
 echo "Done for gantt.html"
 
 # Make gantt_mermaid.png
 /usr/bin/google-chrome --headless --incognito --disable-gpu --window-size=1600,900 --screenshot=gantt_mermaid.png ./gantt.html
+sleep 2
 echo "Done for gantt_mermaid.png"
 
 exit 0
