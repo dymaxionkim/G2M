680016a46eea1fac01f04d204602545f6083da86
dymaxionkim <dymaxion.kim@gmail.com>
2020-05-03 22:25:28 +0900

    V03 (윈도우용까지 오류수중 완료)

--- g2m.bat
+++ g2m.bat
@@ -1,2 +1,11 @@
-g2m.exe
-start chrome --headless --incognito --disable-gpu --window-size=1600,900 --screenshot=C:/git/G2M/gantt_mermaid.png C:/git/G2M/gantt.html
+"C:\Program Files (x86)\GanttProject-2.8\ganttproject.exe" -export csv D:/git/G2M/gantt.gan
+timeout 2 > NUL
+
+D:/git/G2M/g2m.exe
+timeout 2 > NUL
+
+start chrome --headless --incognito --disable-gpu --window-size=1600,900 --screenshot=D:/git/G2M/gantt_mermaid.png D:/git/G2M/gantt.html
+timeout 2 > NUL
+
+"C:\Program Files (x86)\GanttProject-2.8\ganttproject.exe" -export png D:/git/G2M/gantt.gan
+timeout 2 > NUL
